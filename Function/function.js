b0f704b833835fedc049de9a15fb03a0ca699436
Prince Chaudhary <pchaudharyskn@gmail.com>
2024-03-20 07:40:57 +0000

    function

---
+++ Function/function.js
@@ -0,0 +1,16 @@
+
+
+
+// function sayMyName(){
+// console.log("j")
+// console.log("k")
+// }
+// sayMyName();
+// function addTwoNumber(number1,number2){
+//    console.log(number1+number2);
+// }
+// addTwoNumber(3,4);
+function loginUser(username){
+    return `{username}` just logged in
+}
+console.log(loginUser("prince"))
